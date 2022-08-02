c042a4d8649831bb3816fc5ea1ad68ea18b54466
ssjeong <ssjeong920801@gmail.com>
2022-08-03 01:59:31 +0900

    [프뚜] 자식 컴포넌트에서 부모 데이터 값 사용하기 (props)

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <template>
-  <div v-if="false">
-    <RefArray />
-  </div>
-  <div v-else>
-    <ReactiveArray />
-  </div>
+  <!-- 2022.08.03[프뚜]: 06. :자식에 사용될 변수명 = 부모에서 넘겨줄 변수명 -->
+  <MyInfo
+      :info = info
+  />
 </template>
 
 <script>
-import RefArray from './components/list/RefArray.vue';
-import ReactiveArray from './components/list/ReactiveArray.vue';
+import { ref } from 'vue';
+
+// 2022.08.03[프뚜]: 03. 자식 컴포넌트 import
+import MyInfo from './components/props/MyInfo.vue';
 
 export default {
   setup() {
+    // 2022.08.03[프뚜]: 05. 자식 컴포넌트가 사용 할 변수 생성
+    const info = ref({
+      'name': '프뚜',
+      'job': 'programmer',
+      'hobby': 'gym',
+    });
 
+    return {
+      info
+    };
   },
 
+  // 2022.08.03[프뚜]: 04. 자식 컴포넌트 연동
   components: {
-    RefArray,
-    ReactiveArray
+    MyInfo,
   }
 }
 </script>
---
+++ src/components/props/MyInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- 2022.08.03[프뚜]: 02. 부모에서 받아 온 데이터 출력 -->
+  <div>이름 ==> {{ info.name }}</div>
+  <div>취미 ==> {{ info.job }}</div>
+  <div>직업 ==> {{ info.hobby }}</div>
+</template>
+
+<script>
+export default {
+  // 2022.08.03[프뚜]: 01. 부모에서 받아 온 데이터를 매핑 -> info라는 명으로 받아 올 예정
+  props: [
+    'info'
+  ],
+  setup() {
+
+  },
+};
+</script>
+
+<style>
+
+</style>
