6f0baa54473b610c802070b17e84174def64e24d
ssjeong <ssjeong920801@gmail.com>
2022-08-03 17:55:24 +0900

    [프뚜] 자식 컴포넌트에서 부모 function 사용하기 (emit, emits)

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,39 @@
 <template>
-  <!-- 2022.08.03[프뚜]: 06. :자식에 사용될 변수명 = 부모에서 넘겨줄 변수명 -->
+  <!-- 2022.08.03[프뚜]: 07. 자식에서 만든 변수명, 연결 할 function 명 -->
   <MyInfo
-      :info = info
+      @setInfo = setInfo
   />
+
+  <!-- 2022.08.03[프뚜]: 08. 자식에서 받아온 데이터 출력 -->
+  <div>이름 ==> {{ info.name }}</div>
+  <div>취미 ==> {{ info.job }}</div>
+  <div>직업 ==> {{ info.hobby }}</div>
 </template>
 
 <script>
 import { ref } from 'vue';
 
 // 2022.08.03[프뚜]: 03. 자식 컴포넌트 import
-import MyInfo from './components/props/MyInfo.vue';
+import MyInfo from './components/emit/EmitInfo.vue';
 
 export default {
   setup() {
-    // 2022.08.03[프뚜]: 05. 자식 컴포넌트가 사용 할 변수 생성
-    const info = ref({
-      'name': '프뚜',
-      'job': 'programmer',
-      'hobby': 'gym',
+    // 2022.08.03[프뚜]: 05. 자식 컴포넌트에서 받아온 데이터 출력하기 위해 초기화
+    let info = ref({
+        'name': '',
+        'job': '',
+        'hobby': ''
     });
 
+    // 2022.08.03[프뚜]: 06. 자식컴포넌트와 연동 할 function 생성
+    const setInfo = (param) => {
+      console.log(param);
+      info = info.value = param;
+    };
+
     return {
-      info
+      info,
+      setInfo,
     };
   },
 
---
+++ src/components/emit/EmitInfo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>자식 컴포넌트</div>
+</template>
+
+<script>
+export default {
+  // 2022.08.03[프뚜]: 01. 자식 컴포넌트에서 부모 컴포넌트의 함수를 실행
+  setup(props, context) {
+    context.emit('setInfo', {
+      'name': '프뚜',
+      'job': 'programmer',
+      'hobby': 'gym',
+    });
+  }, // setup
+
+  // 2022.08.03[프뚜]: 02. 자식 컴포넌트에서 부모 컴포넌트의 emit명 선언
+  emits: [
+      'setInfo'
+  ], // emits
+};
+</script>
+
+<style>
+
+</style>
