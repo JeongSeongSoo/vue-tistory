6a41cba67d8fa3f8f8bd75a5e6e7e26dd7ad3a96
ssjeong <ssjeong920801@gmail.com>
2022-08-03 16:25:06 +0900

    [프뚜] 자식 컴포넌트에서 부모 데이터 값 사용하기 (props) > props 데이터 값 변경 불가

--- src/components/props/MyInfo.vue
+++ src/components/props/MyInfo.vue
@@ -8,12 +8,20 @@
 <script>
 export default {
   // 2022.08.03[프뚜]: 01. 부모에서 받아 온 데이터를 매핑 -> info라는 명으로 받아 올 예정
-  props: [
-    'info'
-  ],
-  setup() {
+  props: {
+    info: {
+      type: Object,
+      required: true
+    }
+  }, // props
 
-  },
+  // 2022.08.04[프뚜]: props, context 파라미터를 받을 수 있음
+  setup(props) {
+    console.log(props.info);
+
+    // 2022.08.04[프뚜]: props로 받아 온 데이터는 변경이 불가능
+    // props.info.name = '부모데이터값변경불가';
+  }, // setup
 };
 </script>
 
