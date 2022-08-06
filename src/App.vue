e54136ea9936b56c2c1f1d650639de49eaff482c
ssjeong <ssjeong920801@gmail.com>
2022-08-06 17:37:38 +0900

    [프뚜] 태그 안에 이벤트가 연속적으로 일어나는 현상 (이벤트 버블링)  - yarn install  - npm i

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <div>App.vue 입니다.</div>
-
-  <!-- 2022.08.04[프뚜]: router-view 페이지가 보여질 태그 생성 -->
-  <router-view />
+  <!-- 2022.08.06[프뚜]: div 안에 같은 이벤트 프로퍼티를 사용하는 태그 생성 -->
+  <div @click="divClick()">
+    <input type="button" @click.stop="buttonClick()" value="버튼" />
+  </div>
 </template>
 
 <script>
 export default {
   setup() {
+    // 2022.08.06[프뚜]: div를 클릭하면 실행되는 method
+    const divClick = () => {
+      console.log("divClick");
+    };
 
+    // 2022.08.06[프뚜]: button을 클릭하면 실행되는 method
+    const buttonClick = () => {
+      console.log("buttonClick");
+    };
+
+    return {
+      divClick,
+      buttonClick
+    }
   },
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  // 2022.08.05[ssjeong]: ESLint false -> 단일 단어 컴포넌트 사용 가능
   lintOnSave: false
 });
