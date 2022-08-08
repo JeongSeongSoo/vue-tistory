9af3bdd2e8fbb5b857b1852777e0d572b8815ee4
ssjeong <ssjeong920801@gmail.com>
2022-08-08 22:21:36 +0900

    [프뚜] 컴포넌트 라이프사이클 훅 사용하기2  - yarn install  - npm i  - https://v3.vuejs-korea.org/guide/instance.html#%E1%84%85%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%91%E1%85%B3%E1%84%89%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%8F%E1%85%B3%E1%86%AF-%E1%84%83%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%8B%E1%85%A5%E1%84%80%E1%85%B3%E1%84%85%E1%85%A2%E1%86%B7

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   App.vue
 
-  <!-- 2022.08.08[프뚜]: 03. onBeforeMount 이후 onMounted 이전 실행됨 -->
-  {{ loadTemplate() }}
   <br/><br/>
   {{ value }}
+
+  <!-- 2022.08.08[프뚜]: update를 테스트하기 위해 state값 변경 input 추가 -->
   <br/><br/>
   <input type="button" @click="change()" value="바꾸기"/>
 </template>
 
 <script>
-import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted, onErrorCaptured, onRenderTracked, onRenderTriggered } from 'vue';
+import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted } from 'vue';
 
 export default {
   setup() {
@@ -22,61 +22,42 @@ export default {
       console.log('onBeforeMount');
     });
 
-    // 2022.08.08[프뚜]: 04. 템플릿과 렌더링 된 DOM에 접근할 수 있음
+    // 2022.08.08[프뚜]: 03. 템플릿과 렌더링 된 DOM에 접근할 수 있음
     onMounted(() => {
       console.log('onMounted');
     });
 
-    // 2022.08.08[프뚜]: 05. DOM이 패치되기 전에 데이터가 변경될 때 호출됨
-    onBeforeUpdate(() => {
-      console.log('onBeforeUpdate');
-    });
-
-    // 2022.08.08[프뚜]: 06. 데이터가 변경되어 DOM이 렌더링되고 패치된 후에 호출됨
-    onUpdated(() => {
-      console.log('onUpdated');
-    });
-
-    // 2022.08.08[프뚜]: 07. 컴포넌트 인스턴스가 마운트 해제 되기 직전에 호출됨
-    onBeforeUnmount(() => {
-      console.log('onBeforeUnmount');
-    });
-
-    // 2022.08.08[프뚜]: 08. 컴포넌트 인스턴스가 마운트 해제 된 후에 호출됨
-    onUnmounted(() => {
-      console.log('onUnmounted');
-    });
-
-    // 2022.08.08[프뚜]: 09. 자손 컴포넌트의 에러가 포착(capture)될 때 호출됨
-    onErrorCaptured(() => {
-      console.log('onErrorCaptured');
-    });
-
-    // 2022.08.08[프뚜]: 10. 가상 DOM의 재렌더링이 추적될 때 호출됨
-    onRenderTracked(() => {
-      console.log('onRenderTracked');
-    });
-
-    // 2022.08.08[프뚜]: 11. 가상 돔의 재렌더링이 트리거 될 때 호출됨
-    onRenderTriggered(() => {
-      console.log('onRenderTriggered');
-    });
-
-    const loadTemplate = () => {
-      console.log('loadTemplate');
-    };
-
+    // 2022.08.08[프뚜]: state를 변경하기 위해 임시 변수 및 메소드 생성
     const value = ref('프뚜');
     const change = () => {
       value.value = '먹뚜';
     };
 
+    // 2022.08.08[프뚜]: 04. DOM이 패치되기 전에 데이터가 변경될 때 호출됨
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate');
+    });
+
+    // 2022.08.08[프뚜]: 05. 데이터가 변경되어 DOM이 렌더링되고 패치된 후에 호출됨
+    onUpdated(() => {
+      console.log('onUpdated');
+    });
+
+    // 2022.08.08[프뚜]: 06. 컴포넌트 인스턴스가 마운트 해제 되기 직전에 호출됨
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount');
+    });
+
+    // 2022.08.08[프뚜]: 07. 컴포넌트 인스턴스가 마운트 해제 된 후에 호출됨
+    onUnmounted(() => {
+      console.log('onUnmounted');
+    });
+
     return {
-      loadTemplate,
       value,
       change,
     }
-  },
+  }, // setup
 }
 </script>
 
