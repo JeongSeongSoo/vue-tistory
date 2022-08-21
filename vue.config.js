18dc00ebd47175ff435d3f4f0c617db3c743287c
ssjeong <ssjeong@wbsoft.kr>
2022-08-21 20:06:28 +0900

    [프뚜] vuex mutations와 actions 사용하기

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  // 2022.08.05[ssjeong]: ESLint false -> 단일 단어 컴포넌트 사용 가능
+  // 2022.08.05[프뚜]: ESLint false -> 단일 단어 컴포넌트 사용 가능
   lintOnSave: false
 });
